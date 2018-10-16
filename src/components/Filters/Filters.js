2ddb55cc24a2666e8aae6ee72abc5e34c496273d
Victor <31951646+kanoru3101@users.noreply.github.com>
2018-10-16 13:53:32 +0300

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -9,7 +9,7 @@ const Filters = ({
     changeFilter
 }) =>(
     <div className={s.container}>
-        <p className={s.title}><h3>Filters: </h3></p>
+        <h3><p className={s.title}>Filters: </p></h3>
         <button className={s.btn} title="All todo" onClick={() => changeFilter("all")}>&#9776;</button>
         <button className={s.btn} title="completed todo" onClick={() => changeFilter("completed")}>&#10004;</button>
         <button className={s.btn} title="didn`t completed todo" onClick={() => changeFilter("not completed")}>&#10008;</button>
